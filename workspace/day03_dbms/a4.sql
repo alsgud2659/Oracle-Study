6b092f2a5d2c797bd232a4b35e2eec0740aa62a6
alsgud2659 <alsgud2659@naver.com>
2022-07-05 13:45:25 +0900

    CRUD

---
+++ workspace/day03_dbms/a4.sql
@@ -0,0 +1,13 @@
+CREATE TABLE SALGRADE(
+	GRADE NUMBER,
+	LOSAL NUMBER,
+	HISAL NUMBER
+);
+
+insert into salgrade values (1, 700, 1200);
+insert into salgrade values (2, 1201, 1400);
+insert into salgrade values (3, 1401, 2000);
+insert into salgrade values (4, 2001, 3000);
+insert into salgrade values (5, 3001, 9999);
+
+commit;
